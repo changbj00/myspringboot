d6a0b8f4d4731e2e3d1558c6188e5315c2d87a2b
chang00 <1127701980@qq.com>
2020-05-29 16:42:26 +0800

    单元测试

--- src/test/java/com/myspringboot/service/PersonServiceTest.java
+++ src/test/java/com/myspringboot/service/PersonServiceTest.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {myspringbootApplication.class,MockServletContext.class})
 public class PersonServiceTest {
